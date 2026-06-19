dae3a60538d07280beb12462e955015375b639f5
Mesa10GCS <vincenzoggggw@gmail.com>
2026-06-18 22:27:59 -0300

    Update punto1.c

--- lib/punto1/punto1.c
+++ lib/punto1/punto1.c
@@ -19,7 +19,7 @@ void mPaP_Init(motores_t* mpap){
     if(mpap->puerto== GPIOC) RCC->APB2ENR|=RCC_APB2ENR_IOPCEN;
     if(mpap->puerto== GPIOD) RCC->APB2ENR|=RCC_APB2ENR_IOPDEN;
     if(mpap->puerto== GPIOE) RCC->APB2ENR|=RCC_APB2ENR_IOPEEN;
-        for(int i=0; i<7; i++){
+        for(int i=0; i<4; i++){
         if(mpap->pines[i]<8){ 
             mpap->puerto->CRL&=~(0xf<<(mpap->pines[i]*4));
             mpap->puerto->CRL|=(0x1<<mpap->pines[i]*4);
@@ -31,15 +31,28 @@ void mPaP_Init(motores_t* mpap){
 }
 
 void mPap_Steps(motores_t* mpap){
+    int pasos;
+    int gradofinal;
     if(mpap->grado_final>mpap->grado_inicial){
-        int gradofinal = mpap->grado_final- mpap->grado_inicial;
-        int pasos= gradofinal/0.9;
+        gradofinal = mpap->grado_final- mpap->grado_inicial;
+        pasos= gradofinal/0.9;
         for(int i=0; i < (pasos); i++){
-        for(int j; j<8; j++){
-            if(pasosc[i%8][j])GPIOA->BSRR|=(1<<mpap->pines[j]);
+        for(int j=0; j<4; j++){
+            if(pasosc[i%8][j])mpap->puerto->BSRR|=(1<<mpap->pines[j]);
             else GPIOA ->BSRR |=(1<<(mpap->pines[j]+16)); 
         }
+
+        }
     }
-    
-    }
+
+    if(mpap->grado_inicial>mpap->grado_final){
+        gradofinal = mpap->grado_inicial - mpap->grado_final;
+        pasos = gradofinal/0.9;
+        for(int i=pasos; i > 0; i--){
+        for(int j=0; j<4; j++){
+            if(pasosc[i%8][j])mpap->puerto->BSRR|=(1<<mpap->pines[j]);
+            else GPIOA ->BSRR |=(1<<(mpap->pines[j]+16)); 
+        }
+    }    
+}
 }
